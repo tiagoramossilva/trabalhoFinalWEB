b74c32451578803a5e67f399609b1635b9a9024e
Tiago Ramos Silva <tiagoramossilva40@gmail.com>
2024-12-04 11:42:21 -0300

    Adicionando pasta firebase ao .gitignore

--- db/firebase.js
+++ db/firebase.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
-const { initializeApp } = require('firebase/app');
-const { getFirestore } = require('firebase/firestore');
+require("dotenv").config();
+const { initializeApp } = require("firebase/app");
+const { getFirestore } = require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyBtztcs6VZ1CcganeLhQUYB9snZM--wKvU",
+  authDomain: "aulaweb-a0d1b.firebaseapp.com",
+  projectId: "aulaweb-a0d1b",
+  storageBucket: "aulaweb-a0d1b.firebasestorage.app",
+  messagingSenderId: "814702770637",
+  appId: "1:814702770637:web:55a390751489f78d62ac84",
+  measurementId: "G-ZBM35H9RQW",
 };
 
 const app = initializeApp(firebaseConfig);
